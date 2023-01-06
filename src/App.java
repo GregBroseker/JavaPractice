bb969d462fc22a794b07bb3e78a3875fdcfb796a
BroskaH95 <gregbroseker95@gmail.com>
2023-01-06 00:44:49 -0500

    2nd commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class App {//High level public class must match .java file name
+    public static void main(String[] args) {// main method that accepts 2 parameters
+      System.out.println("Hello World!");//print Hello World to console
+
+      int time = 20;//declare variable as int data type
+      if (time < 18) {//establish condition asking if time variable is less than 18
+        System.out.println("Good day.");//if true prints Good day.
+      } else {
+        System.out.println("Good evening.");//else executes when false and prints Good evening instead.
+      }     
     }
-}
+  }
